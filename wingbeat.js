64197bb6cd0b0defbd846e94046d90e3da607867
Mehmet Ali Şipi <mehmetalisipi@gmail.com>
2023-09-07 22:47:38 +0300

    Fix: Ignoring the max count of allowed hastag

--- wingbeat.js
+++ wingbeat.js
@@ -126,13 +126,15 @@ svg[data-testid="icon-verified"] {
         }
     },
     removed_spam_tweet_count: 0,
-    remove_spam_tweets: () => {
+    max_hashtag_count: null,
+    remove_spam_tweets: async () => {
+    		wingbeat.max_hashtag_count = Number(await wingbeat.get_config("spam_tweet_value"))
         	document.addEventListener("scroll", () => {
 		    document.querySelectorAll("article").forEach(
 			    tweet => {
 				    let hashtag_count = (tweet.innerText.match(/\#/g)||[]).length
 				    let is_sponsored = !!tweet.querySelector("path[d='M19.498 3h-15c-1.381 0-2.5 1.12-2.5 2.5v13c0 1.38 1.119 2.5 2.5 2.5h15c1.381 0 2.5-1.12 2.5-2.5v-13c0-1.38-1.119-2.5-2.5-2.5zm-3.502 12h-2v-3.59l-5.293 5.3-1.414-1.42L12.581 10H8.996V8h7v7z']")
-				    if (hashtag_count >= 3 && !is_sponsored){
+				    if (hashtag_count > wingbeat.max_hashtag_count && !is_sponsored){
 					    console.info("Wingbeat: Spam Text Removed [" + (++wingbeat.removed_spam_tweet_count) + "]\n" + tweet.innerText)
 					    tweet.remove();
 				    };
